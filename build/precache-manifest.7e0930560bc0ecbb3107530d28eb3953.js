def84b764e0e0b3651b9411339fb330339a228d1
sandeep <dev.sandeep@hotmail.com>
2020-01-24 14:48:50 +0530

    build added

---
+++ build/precache-manifest.7e0930560bc0ecbb3107530d28eb3953.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3c40074da1d93cd949a39e91996929e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4097821804f70ef3792f",
+    "url": "/static/css/main.019fb6c9.chunk.css"
+  },
+  {
+    "revision": "f8e512556eb54ceef658",
+    "url": "/static/js/2.397390a6.chunk.js"
+  },
+  {
+    "revision": "4097821804f70ef3792f",
+    "url": "/static/js/main.70bb4c8b.chunk.js"
+  },
+  {
+    "revision": "c375f48ae8a24969f898",
+    "url": "/static/js/runtime-main.6c504066.js"
+  }
+]);
