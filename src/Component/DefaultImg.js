959c34f6bba342df3d83437a4ea8f6f6ce8f0f48
sandeep <dev.sandeep@hotmail.com>
2020-02-05 18:02:20 +0530

    change in picture

--- src/Component/DefaultImg.js
+++ src/Component/DefaultImg.js
@@ -3,6 +3,7 @@
 import React, { useState, useEffect, setState } from 'react'
 import UseBaseContext from '../ContextApi/UseBaseContext'
 import UriCall from '../ContextApi/UrlCall'
+import {IMAGE_DEFAULT} from './../Utility/Constants'
 
 function DefaultImg() {
 
@@ -10,7 +11,7 @@ function DefaultImg() {
     /* all of the maon content goes here  */
     return (
         <section className="defaultimg-copmponent">
-            <img className="left-img" src="https://png2.cleanpng.com/sh/59b7cbbe1a4cd8efd56650679e778ad6/L0KzQYm3VsA2N5h4hJH0aYP2gLBuTfZqdpJze9tqbD3kc7T2lf51cZ9sRdNsY3B4fsXojwQubJZ4g59qY3Pog8T2kvlme146eqMCMUW0RIS4g8A5QV89SaQBOUe3RoK8UsgzO2o4UKI7MEO7PsH1h5==/kisspng-financial-accounting-accountant-desk-accessories-5b17151431c089.8126974615282393802038.png" />
+            <img className="left-img" src={IMAGE_DEFAULT} />
         </section >
     );
 }
--- src/Utility/Constants.js
+++ src/Utility/Constants.js
@@ -1,3 +1,4 @@
 export const STATUS_INIT = 1;
 export const STATUS_PROCESS = 2;
 export const STATUS_TRANSACTION = 3;
+export const IMAGE_DEFAULT = 'https://image.freepik.com/free-photo/office-workplace-with-keypad-glasses_23-2147833237.jpg';
